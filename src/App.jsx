06235d67387ab96ba237cbdffaf49e78651cb551
Jason McGrath <jnomad21@yahoo.com>
2023-10-09 15:24:42 -0700

    nav1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Jason McGrath</h1>
-      </header>
-    </div>
+    <>
+    <NavBar />
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import './NavBar.css'
+export default function NavBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <h4>About Me</h4>
+                    </li>
+                    <li>
+                        <h4>Projects</h4>
+                    </li>
+                    <li>
+                        <h4>Skills</h4>
+                    </li>
+                    <li>
+                        <h4>Contact</h4>
+                    </li>
+                </ul>
+            </nav>
+            
+        </div>
+    )
+}
