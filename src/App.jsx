413296248f0a3673d3e56557ecd54ae67f6f5827
Jason McGrath <jnomad21@yahoo.com>
2023-10-09 15:44:14 -0700

    added Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "./components/NavBar";
+import './index.css'
 
 function App() {
   return (
     <>
     <NavBar />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   );
 }
