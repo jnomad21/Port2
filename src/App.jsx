79ca111949c9cc06b8adc556973978fdf819eb20
Jason McGrath <jnomad21@yahoo.com>
2023-10-20 13:10:28 -0700

    alpha1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import NavBar from "./components/NavBar";
+import Splash from "./pages/SplashPage";
 import './index.css'
 
 function App() {
   return (
     <>
+    <Splash />
     <NavBar />
     <h1>
       Jason McGrath
     </h1>
+    <h3>A developer's heart</h3>
     </>
   );
 }
---
+++ src/pages/SplashPage.jsx
@@ -0,0 +1,11 @@
+import './SplashPage.css'
+export default function Splash() {
+    return (
+        <body className="splash-body center">
+            
+        <div>
+            Are you ready to meet the Jason McGrath???
+        </div>
+        </body>
+    )
+}
