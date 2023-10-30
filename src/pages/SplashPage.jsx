34878c79ac51c218f586c02b2af112be179fe73f
Jason McGrath <jnomad21@yahoo.com>
2023-10-30 14:51:17 -0700

    splashpage1

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -3,7 +3,7 @@ export default function Splash() {
     return (
         <body className="splash-body center">
             
-        <div>
+        <div className='splash-alpha'>
             Are you ready to meet the Jason McGrath???
         </div>
         </body>
