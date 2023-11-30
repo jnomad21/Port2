cdbbe743be7297a5e24e0538c9293b0ff3965526
Jason McGrath <jnomad21@yahoo.com>
2023-11-29 16:05:46 -0800

    Quote Translator Component Formatting1

--- src/components/QuoteTranslator.jsx
+++ src/components/QuoteTranslator.jsx
@@ -15,18 +15,21 @@ export default function QuoteTranslator() {
         };
 
     return (
-        <div 
-            className='quote-container' 
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}>
-            <div className="quote-raw">
-                <p className='quote-text'>
-                    {isHovered 
-                    ? <span className='quote-en'>"What we think of as impossible is just something that has not been engineered yet"</span>
-                    : <span className='quote-fr'>"Ce que nous considérons comme impossible est simplement quelque chose qui n'a pas encore été conçu."</span>}
-                </p>
-                <span className="quote-attr">-JM</span>
+        <>
+        <div className='quote-body'>
+            <div 
+                className='quote-container' 
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}>
+                <div className="quote-raw">
+                    <p className='quote-text'>
+                        {isHovered 
+                        ? <span className='quote-en'>"What we think of as impossible is just something that has not been engineered yet"</span>
+                        : <span className='quote-fr'>"Ce que nous considérons comme impossible est simplement quelque chose qui n'a pas encore été conçu."</span>}
+                    </p>
+                    <span className="quote-attr">-JM</span>
+                </div>
             </div>
-        </div>
+        </div></>
     )
 }
